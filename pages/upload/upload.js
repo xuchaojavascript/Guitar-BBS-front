79505211eae45b132ec03b0418c0049f2c7d2e86
yichar <1454030573@qq.com>
2019-10-08 23:16:49 +0800

    小程序首页头部菜单
    小程序底部导航、首页头部搜索、菜单

--- pages/upload/upload.js
+++ pages/upload/upload.js
@@ -1,37 +1,27 @@
 //index.js
 //获取应用实例
 const app = getApp()
+
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    searchData: [],
-    menuList: [
-      { title: '问答', text: '遇事不决该辞职辞职' },
-      { title: '今日练习', text: '今天你打卡签退了吗' },
-      { title: '秀技术', text: '大神都是这种傻屌吗' },
-      { title: '资源', text: '多读书多运动多吃饭多睡觉' },
-      { title: '求谱', text: '谱子从来都是什么鬼' }
-    ]
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
   onLoad: function () {
-    this.setData({
-      searchData: this.search.bind(this)
-    })
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -53,26 +43,12 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  },
-  selectResult (e) {
-    console.log(1)
-    console.log('select result', e.detail)
-  },
-  search (value) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve([{ text: '搜索结果', value: 1 }, { text: '搜索结果2', value: 2 }])
-      }, 200)
-    })
-  },
-  clickMenu (e) {
-    console.log(e.currentTarget.dataset.index)
   }
 })
